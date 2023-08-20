e5022e5b03730ab7cbe5e3bb3023803442e795fd
mikitosina1 <mikitosina@mail.ru>
2023-08-21 01:15:24 +0300

    overflow for main pages articles

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,94 @@
 @section('head_title')Home page @endsection
 @section('where__am__I')You're on a home page @endsection
 
-@section('main_content')
-		<div class="content__cloud">
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-            A delectus, dolor eius error facilis id praesentium quia tempore? 
-            At culpa distinctio dolorum ex exercitationem illo libero, nobis tempore veniam voluptates.
-		</div>
+@section('content')
+<div class="cloud main_content ">
+	<div class="item">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta magnam ullam natus amet numquam ea. 
+    Facilis nulla culpa tempore voluptate et, odio iusto aut cum, repudiandae, dolores possimus eligendi delectus.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dolorum facilis repudiandae dicta, natus harum quam fugit necessitatibus ut.
+    Tempore veniam deleniti hic quisquam architecto ab fuga aliquam illum nam.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta magnam ullam natus amet numquam ea. 
+    Facilis nulla culpa tempore voluptate et, odio iusto aut cum, repudiandae, dolores possimus eligendi delectus.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dolorum facilis repudiandae dicta, natus harum quam fugit necessitatibus ut.
+    Tempore veniam deleniti hic quisquam architecto ab fuga aliquam illum nam.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta magnam ullam natus amet numquam ea. 
+    Facilis nulla culpa tempore voluptate et, odio iusto aut cum, repudiandae, dolores possimus eligendi delectus.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dolorum facilis repudiandae dicta, natus harum quam fugit necessitatibus ut.
+    Tempore veniam deleniti hic quisquam architecto ab fuga aliquam illum nam.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta magnam ullam natus amet numquam ea. 
+    Facilis nulla culpa tempore voluptate et, odio iusto aut cum, repudiandae, dolores possimus eligendi delectus.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dolorum facilis repudiandae dicta, natus harum quam fugit necessitatibus ut.
+    Tempore veniam deleniti hic quisquam architecto ab fuga aliquam illum nam.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta magnam ullam natus amet numquam ea. 
+    Facilis nulla culpa tempore voluptate et, odio iusto aut cum, repudiandae, dolores possimus eligendi delectus.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dolorum facilis repudiandae dicta, natus harum quam fugit necessitatibus ut.
+    Tempore veniam deleniti hic quisquam architecto ab fuga aliquam illum nam.
+	</div>
+	<div class="show_more">Show more</div>
+</div>
+<div class="cloud main_content">
+	<div class="item">
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta magnam ullam natus amet numquam ea. 
+		Facilis nulla culpa tempore voluptate et, odio iusto aut cum, repudiandae, dolores possimus eligendi delectus.
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dolorum facilis r
+		epudiandae dicta, natus harum quam fugit necessitatibus ut.
+		Tempore veniam deleniti hic quisquam architecto ab fuga aliquam illum nam.
+	</div>
+	<div class="show_more ">Show more</div>
+</div>
+<div class="cloud main_content">
+	<div class="item">
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta magnam ullam natus amet numquam ea. 
+		Facilis nulla culpa tempore voluptate et, odio iusto aut cum, repudiandae, dolores possimus eligendi delectus.
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dolorum facilis repudiandae dicta, natus harum quam fugit necessitatibus ut.
+		Tempore veniam deleniti hic quisquam architecto ab fuga aliquam illum nam.
+	</div>
+	<div class="show_more">Show more</div>
+</div>
+<div class="cloud main_content">
+	<div class="item">
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta magnam ullam natus amet numquam ea. 
+		Facilis nulla culpa tempore voluptate et, odio iusto aut cum, repudiandae, dolores possimus eligendi delectus.
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dolorum facilis repudiandae dicta, natus harum quam fugit necessitatibus ut.
+		Tempore veniam deleniti hic quisquam architecto ab fuga aliquam illum nam.
+	</div>
+	<div class="show_more">Show more</div>
+</div>
+<div class="cloud main_content">
+	<div class="item">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta magnam ullam natus amet numquam ea. 
+    Facilis nulla culpa tempore voluptate et, odio iusto aut cum, repudiandae, dolores possimus eligendi delectus.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dolorum facilis repudiandae dicta, natus harum quam fugit necessitatibus ut.
+    Tempore veniam deleniti hic quisquam architecto ab fuga aliquam illum nam.
+	</div>
+	<div class="show_more">Show more</div>
+</div>
+<div class="cloud main_content">
+	<div class="item">
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta magnam ullam natus amet numquam ea. 
+		Facilis nulla culpa tempore voluptate et, odio iusto aut cum, repudiandae, dolores possimus eligendi delectus.
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dolorum facilis repudiandae dicta, natus harum quam fugit necessitatibus ut.
+		Tempore veniam deleniti hic quisquam architecto ab fuga aliquam illum nam.
+	</div>
+  <div class="show_more">Show more</div>
+</div>
+<div class="cloud main_content">
+	<div class="item">
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta magnam ullam natus amet numquam ea. 
+		Facilis nulla culpa tempore voluptate et, odio iusto aut cum, repudiandae, dolores possimus eligendi delectus.
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dolorum facilis repudiandae dicta, natus harum quam fugit necessitatibus ut.
+		Tempore veniam deleniti hic quisquam architecto ab fuga aliquam illum nam.
+	</div>
+  <div class="show_more">Show more</div>
+</div>
+<div class="cloud main_content">
+	<div class="item">
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta magnam ullam natus amet numquam ea. 
+		Facilis nulla culpa tempore voluptate et, odio iusto aut cum, repudiandae, dolores possimus eligendi delectus.
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dolorum facilis repudiandae dicta, natus harum quam fugit necessitatibus ut.
+		Tempore veniam deleniti hic quisquam architecto ab fuga aliquam illum nam.
+	</div>
+	<div class="show_more">Show more</div>
+</div>
 @endsection
