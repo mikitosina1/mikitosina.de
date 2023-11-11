a8eee48e96c1f9d7cd35505422703a1141e5abb6
mikitosina1 <mikitosina@mail.ru>
2023-11-11 01:36:50 +0100

    user block styling

--- resources/views/layouts/user_cloud.blade.php
+++ resources/views/layouts/user_cloud.blade.php
@@ -1,21 +1,23 @@
 @section('user_cloud')
 <div class="right_cloud">
 	<div class="user_cloud">
-		@if(auth()->check())
-			<a href="{{ route('personalPage', ['id' => auth()->user()->id]) }}" class="cloud_item">{{ __('user_cloud.personalPage') }}</a>
-			<a class="cloud_item" href="{{ route('logout') }}">
-				<form method="POST" action="{{ route('logout') }}">
-					@csrf
-					<button type="submit" class="btn logout">
-						{{ __('user_cloud.logout') }}
-					</button>
-				</form>
-			</a>
-		@else
-			<div class="login_block">
-				<a class="nav_item" href="{{ route('register') }}">{{ __('user_cloud.register') }}</a> or <a class="nav_item" href="{{ route('login') }}">{{ __('user_cloud.login') }}</a>
-			</div>
-		@endif
+		<div class="auth_btn_block">
+			@if(auth()->check())
+				<a href="{{ route('personalPage', ['id' => auth()->user()->id]) }}" class="cloud_item">{{ __('user_cloud.personalPage') }}</a>
+				<a class="cloud_item" href="javascript:void(0)">
+					<form method="POST" action="{{ route('logout') }}">
+						@csrf
+						<button type="submit" class="btn logout">
+							{{ __('user_cloud.logout') }}
+						</button>
+					</form>
+				</a>
+			@else
+				<div class="login_block">
+					<a class="nav_item" href="{{ route('register') }}">{{ __('user_cloud.register') }}</a> or <a class="nav_item" href="{{ route('login') }}">{{ __('user_cloud.login') }}</a>
+				</div>
+			@endif
+		</div>
 		@include('layouts.message')
 	</div>
 </div>
