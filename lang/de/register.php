54c4215c88ad2cf77b19d3c5667331503933d254
mikitosina1 <mikitosina@mail.ru>
2023-11-12 15:34:53 +0100

    added translations

---
+++ lang/de/register.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'fname' => 'Vorname',
+    'lname' => 'Nachname',
+    'email' => 'E-Mail-Adresse',
+    'pw'    => 'Passwort',
+    '2pw'   => 'Bestätige das Passwort',
+    'photo' => 'Foto',
+    'bio'   => 'Bio',
+    'subm'  => 'Registrieren'
+];
