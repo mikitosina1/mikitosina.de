c312195a9433b52ee796528aa945e1d522d87f80
mikitosina1 <mikitosina@mail.ru>
2024-01-08 00:20:23 +0100

    short adding info for thinking about page 'about'

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,9 +3,9 @@
 @section('head_title')About @endsection
 @section('content')
 	<div>
-		about
 		<div><img src="https://pimcore.com/academy/certificate-validation/badge/18PB765KY2E9XVN" style="max-width: 5em; max-height: 5em;"></div>
-
-		<a href="https://mynickname.com/mikitosina1"><img src="https://mynickname.com/img.php?nick=mikitosina1&sert=2&text=t4" alt="Nickname mikitosina1 is registered!" border="0" style="width: 80%; height: 22px;"/></a>
+		<a href="https://mynickname.com/mikitosina1">
+			<img src="https://mynickname.com/img.php?nick=mikitosina1&sert=2&text=t4" alt="Nickname mikitosina1 is registered!" border="0" style="width: 80%; height: 22px;"/>
+		</a>
 	</div>
 @endsection
