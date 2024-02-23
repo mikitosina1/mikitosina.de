2a69d6a19bf8c7f33c04c1151eb86a107e599bf1
mikitosina1 <mikitosina@mail.ru>
2024-02-23 21:52:08 +0100

    * styles for register and login forms

--- resources/views/pages/users/register.blade.php
+++ resources/views/pages/users/register.blade.php
@@ -99,7 +99,7 @@
 
 		<div class="form-group row mb-0">
 			<div class="col-md-6 offset-md-4">
-				<button type="submit" class="btn btn-primary submit">
+				<button type="submit" class="black">
 					{{ __('register.subm') }}
 				</button>
 			</div>
