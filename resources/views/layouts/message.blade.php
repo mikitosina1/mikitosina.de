e9fe6a00a00ee2d6d8dc7adf3d93e66dbb0365aa
mikitosina1 <mikitosina@mail.ru>
2024-03-06 00:16:02 +0100

    * separates access to $error and to session('error')

--- resources/views/layouts/message.blade.php
+++ resources/views/layouts/message.blade.php
@@ -1,10 +1,16 @@
-@if($errors->any() ||session('errors'))
+@if ($errors->any())
 	<div class="alert alert-danger contact__danger">
-		<ul>
-			@foreach($errors->all() as $error)
-				<li>
-					{{$error}}
-				</li>
+		<ul class="my-0">
+			@foreach ($errors->all() as $error)
+				<li>{{ $error }}</li>
+			@endforeach
+		</ul>
+	</div>
+@elseif (null !== session('errors') && session('errors')->any())
+	<div class="alert alert-danger contact__danger">
+		<ul class="my-0">
+			@foreach (session('errors')->all() as $error)
+				<li>{{ $error }}</li>
 			@endforeach
 		</ul>
 	</div>
@@ -12,6 +18,6 @@
 
 @if(session('success'))
 	<div class="alert alert-success success__message">
-		{{session('success')}}
+		{{ session('success') }}
 	</div>
 @endif
