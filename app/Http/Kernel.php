b4486f667cb2ce2bec786793e7a913c30c163320
mikitosina1 <mikitosina@mail.ru>
2024-03-06 00:15:03 +0100

    * for errors in middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -23,6 +23,7 @@ class Kernel extends HttpKernel
 		\App\Http\Middleware\TrimStrings::class,
 		\Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
 		\App\Http\Middleware\SetLocale::class,
+		\Illuminate\View\Middleware\ShareErrorsFromSession::class,
 	];
 
 	/**
